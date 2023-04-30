3429121f3ea7f6d7d8912787d300b7f8fbe63413
lucasferreiralimax <lucasferreiralimax@gmail.com>
2023-04-29 22:30:30 -0300

    add version in title

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import reactLogo from './assets/react.svg'
 import './App.css'
+import pkg from '../package.json'
 
 // import ReactTechsLogos from './components/ReactTechsLogos'
 import ReactTechsLogos from 'react-techs-logos'
 
 function App() {
+  const version: any = pkg.version;
   return (
     <>
       <div>
@@ -12,7 +14,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>react-techs-logos@0.0.0</h1>
+      <h1>react-techs-logos@{ version }</h1>
       <ReactTechsLogos />
     </>
   )
