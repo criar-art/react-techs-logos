9a841303b2b71b6b5699954dd87cad370ed1aa98
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-02-06 19:08:01 -0300

    improvement and refactory style page for components show

--- src/App.tsx
+++ src/App.tsx
@@ -21,33 +21,50 @@ function App() {
       </code>
       <h2>Usage</h2>
       <code>
-        import ReactTechsLogs from 'react-techs-logos';
+        import ReactTechsLogs from 'react-techs-logos'
       </code>
-      <h2 className='title'>Single tech</h2>
-      <code>
-        {`<ReactTechsLogos name="react" />`}
-      </code>
-      <ReactTechsLogos name="react" />
-      <h2 className='title'>List of techs limited</h2>
-      <code>
-        {`<ReactTechsLogos list={['vue', 'react', 'angular', 'ember']} />`}
-      </code>
-      <ReactTechsLogos list={['vue', 'react', 'angular', 'ember']} />
-      <h2 className='title'>List of techs</h2>
-      <code>
-        {`<ReactTechsLogos />`}
-      </code>
-      <ReactTechsLogos />
-      <h2 className='title'>List of techs hiddenLogos items</h2>
-      <code>
-        {`<ReactTechsLogos hiddenLogos={['android', 'apple', 'vue', 'react', 'angular', 'ember']} />`}
-      </code>
-      <ReactTechsLogos hiddenLogos={['android', 'apple', 'vue', 'react', 'angular', 'ember']} />
-      <h2 className='title'>List of techs with hiddenLabel</h2>
-      <code>
-        {`<ReactTechsLogos hiddenLabel />`}
-      </code>
-      <ReactTechsLogos hiddenLabel />
+      <div className='content'>
+        <h2 className='title'>Single tech</h2>
+        <code>
+          {`<ReactTechsLogos name="react" />`}
+        </code>
+        <ReactTechsLogos name="react" />
+      </div>
+      <div className='content'>
+        <h2 className='title'>Single tech hiddenLabel</h2>
+        <code>
+          {`<ReactTechsLogos name="react" hiddenLabel />`}
+        </code>
+        <ReactTechsLogos name="react" hiddenLabel/>
+      </div>
+      <div className='content'>
+        <h2 className='title'>List of techs limited</h2>
+        <code>
+          {`<ReactTechsLogos list={['vue', 'react', 'angular', 'ember']} />`}
+        </code>
+        <ReactTechsLogos list={['vue', 'react', 'angular', 'ember']} />
+      </div>
+      <div className='content'>
+        <h2 className='title'>List of techs</h2>
+        <code>
+          {`<ReactTechsLogos />`}
+        </code>
+        <ReactTechsLogos />
+      </div>
+      <div className='content'>
+        <h2 className='title'>List of techs hiddenLogos items</h2>
+        <code>
+          {`<ReactTechsLogos hiddenLogos={['android', 'apple', 'vue', 'react', 'angular', 'ember']} />`}
+        </code>
+        <ReactTechsLogos hiddenLogos={['android', 'apple', 'vue', 'react', 'angular', 'ember']} />
+      </div>
+      <div className='content'>
+        <h2 className='title'>List of techs with hiddenLabel</h2>
+        <code>
+          {`<ReactTechsLogos hiddenLabel />`}
+        </code>
+        <ReactTechsLogos hiddenLabel />
+      </div>
     </>
   )
 }
