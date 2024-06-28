adb30556608f655008b3207acd28586144f76750
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-28 12:59:37 -0300

    chagen theme in dark and colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,71 @@
-import reactLogo from './assets/techs/react.svg'
-import './App.css'
-import pkg from '../package.json'
+import reactLogo from "./assets/techs/react.svg";
+import "./App.css";
+import pkg from "../package.json";
 
-import ReactTechsLogos from './components/ReactTechsLogos'
+import ReactTechsLogos from "./components/ReactTechsLogos";
 // import ReactTechsLogos from 'react-techs-logos'
 
 function App() {
   const version: string = pkg.version;
 
   return (
-    <div data-testid='app-container'>
+    <div data-testid="app-container">
       <div>
-        <a className="logo react" href="https://github.com/criar-art/react-techs-logos" target="_blank">
+        <a
+          className="logo react"
+          href="https://github.com/criar-art/react-techs-logos"
+          target="_blank"
+        >
           <img src={reactLogo} alt="React logo" />
-          <h1>react-techs-logos@{ version }</h1>
+          <h1>react-techs-logos@{version}</h1>
         </a>
       </div>
-      <h2>Install</h2>
-      <code>
-        npm install --save react-techs-logos
-      </code>
-      <h2>Usage</h2>
-      <code>
-        import ReactTechsLogs from 'react-techs-logos'
-      </code>
-      <div className='content'>
-        <h2 className='title'>Single tech</h2>
-        <code>
-          {`<ReactTechsLogos name="react" />`}
-        </code>
+      <div className="content">
+        <h2>Install</h2>
+        <code>npm install --save react-techs-logos</code>
+      </div>
+      <div className="content">
+        <h2>Usage</h2>
+        <code>import ReactTechsLogs from 'react-techs-logos'</code>
+      </div>
+      <div className="content">
+        <h2 className="title">Single tech</h2>
+        <code>{`<ReactTechsLogos name="react" />`}</code>
         <ReactTechsLogos name="react" />
       </div>
-      <div className='content'>
-        <h2 className='title'>Single tech hiddenLabel</h2>
-        <code>
-          {`<ReactTechsLogos name="react" hiddenLabel />`}
-        </code>
-        <ReactTechsLogos name="react" hiddenLabel/>
+      <div className="content">
+        <h2 className="title">Single tech hiddenLabel</h2>
+        <code>{`<ReactTechsLogos name="react" hiddenLabel />`}</code>
+        <ReactTechsLogos name="react" hiddenLabel />
       </div>
-      <div className='content'>
-        <h2 className='title'>List of techs limited</h2>
+      <div className="content">
+        <h2 className="title">List of techs limited</h2>
         <code>
           {`<ReactTechsLogos list={['vue', 'react', 'angular', 'ember']} />`}
         </code>
-        <ReactTechsLogos list={['vue', 'react', 'angular', 'ember']} />
+        <ReactTechsLogos list={["vue", "react", "angular", "ember"]} />
       </div>
-      <div className='content'>
-        <h2 className='title'>List of techs</h2>
-        <code>
-          {`<ReactTechsLogos />`}
-        </code>
+      <div className="content">
+        <h2 className="title">List of techs</h2>
+        <code>{`<ReactTechsLogos />`}</code>
         <ReactTechsLogos />
       </div>
-      <div className='content'>
-        <h2 className='title'>List of techs hiddenLogos items</h2>
+      <div className="content">
+        <h2 className="title">List of techs hiddenLogos items</h2>
         <code>
           {`<ReactTechsLogos hiddenLogos={['android', 'apple', 'vue', 'react', 'angular', 'ember']} />`}
         </code>
-        <ReactTechsLogos hiddenLogos={['android', 'apple', 'vue', 'react', 'angular', 'ember']} />
+        <ReactTechsLogos
+          hiddenLogos={["android", "apple", "vue", "react", "angular", "ember"]}
+        />
       </div>
-      <div className='content'>
-        <h2 className='title'>List of techs with hiddenLabel</h2>
-        <code>
-          {`<ReactTechsLogos hiddenLabel />`}
-        </code>
+      <div className="content">
+        <h2 className="title">List of techs with hiddenLabel</h2>
+        <code>{`<ReactTechsLogos hiddenLabel />`}</code>
         <ReactTechsLogos hiddenLabel />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
