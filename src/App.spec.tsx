aa86f4c0b2b86ae50b71e137b6c848500ac9c3cb
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-07-01 13:36:32 -0300

    test(unit): adjustment test name

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,7 +1,6 @@
 import { describe, expect, it, beforeEach } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import App from './App';
-import pkg from '../package.json'
 
 describe('Should render <App/>', () => {
   beforeEach(() => {
@@ -10,8 +9,7 @@ describe('Should render <App/>', () => {
   it('renders simple App', () => {
     expect(screen.getByTestId('app-container')).toBeInTheDocument()
   });
-  it('render title with version', () => {
-    const version: string = pkg.version;
-    expect(screen.getByText(`react-techs-logos@${version}`)).toBeInTheDocument()
+  it('render title', () => {
+    expect(screen.getAllByText("react-techs-logos")).toBeTruthy()
   });
 });
