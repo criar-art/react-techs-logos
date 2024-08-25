e36a2e61413b872f7699f07eb4d77b145feff8cc
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-08-25 03:25:06 -0300

    create d ts react-techs-logos

---
+++ src/react-techs-logos.d.ts
@@ -0,0 +1,1 @@
+declare module 'react-techs-logos';
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,3 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
-declare module 'react-techs-logos';
 declare module 'darkmode-react-component';
